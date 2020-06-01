7387d406374ead2f5770be231c9313289915d911
Damilare <32745404+veectorh@users.noreply.github.com>
2020-06-01 10:38:46 +0100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,87 +1,119 @@
 
 
-/**
- * In JavaScript, integers beyond +/- 9007199254740991 (available as the
- * Number.MIN_SAFE_INTEGER and NUMBER.MAX_SAFE_INTEGER constants) cannot be
- * accurately represented by the default "Number" type. To see this in action,
- * console.log() out the expected number of grains on square #64:
- *
- * console.log(9223372036854775808);
- * // =>       9223372036854776000
- * //                         ^^^^
- *
- * This is because, in JavaScript, "Number" type integers are actually
- * represented internally as 64-bit floating point numbers. If you want to learn
- * more, see:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
- * http://stackoverflow.com/questions/307179/what-is-javascripts-highest-integer-value-that-a-number-can-go-to-without-losin
- *
- * Historically, this limitation could only be overcome by use third-party
- * implementations of a "bit integer" type like "BigInteger.js"
- * (https://github.com/peterolson/BigInteger.js).
- *
- * However, a new JavaScript arbitrary precision integer type named "BigInt",
- * which you should use to solve this exercise, is in the late stages of
- * ECMAScript standardisation and already available in most major browsers and
- * Node.js >= 10.4.0. For more details see:
- *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
- */
+const { Character, abilityModifier } = require('./index');
 
-
-const {square, total }= require('./index');
-
-describe('Grains', () => {
-  describe('Returns The Number Of Grains On The Square', () => {
-    test('1', () => {
-      expect(square(1).toString()).toEqual('1');
+describe('D&D Character', () => {
+  describe('Ability modifier', () => {
+    test('ability modifier for score 3 is -4', () => {
+      expect(abilityModifier(3)).toEqual(-4);
     });
 
-    test('2', () => {
-      expect(square(2).toString()).toEqual('2');
+    test('ability modifier for score 4 is -3', () => {
+      expect(abilityModifier(4)).toEqual(-3);
     });
 
-    test('3', () => {
-      expect(square(3).toString()).toEqual('4');
+    test('ability modifier for score 5 is -3', () => {
+      expect(abilityModifier(5)).toEqual(-3);
     });
 
-    test('4', () => {
-      expect(square(4).toString()).toEqual('8');
+    test('ability modifier for score 6 is -2', () => {
+      expect(abilityModifier(6)).toEqual(-2);
     });
 
-    test('16', () => {
-      expect(square(16).toString()).toEqual('32768');
+    test('ability modifier for score 7 is -2', () => {
+      expect(abilityModifier(7)).toEqual(-2);
     });
 
-    test('32', () => {
-      expect(square(32).toString()).toEqual('2147483648');
+    test('ability modifier for score 8 is -1', () => {
+      expect(abilityModifier(8)).toEqual(-1);
     });
 
-    test('64', () => {
-      expect(square(64).toString()).toEqual('9223372036854775808');
+    test('ability modifier for score 9 is -1', () => {
+      expect(abilityModifier(9)).toEqual(-1);
     });
 
-    test('square 0 raises an exception', () => {
-      expect(() => square(0)).toThrow(
-        new Error('square must be between 1 and 64')
+    test('ability modifier for score 10 is 0', () => {
+      expect(abilityModifier(10)).toEqual(0);
+    });
+
+    test('ability modifier for score 11 is 0', () => {
+      expect(abilityModifier(11)).toEqual(0);
+    });
+
+    test('ability modifier for score 12 is 1', () => {
+      expect(abilityModifier(12)).toEqual(1);
+    });
+
+    test('ability modifier for score 13 is 1', () => {
+      expect(abilityModifier(13)).toEqual(1);
+    });
+
+    test('ability modifier for score 14 is 2', () => {
+      expect(abilityModifier(14)).toEqual(2);
+    });
+
+    test('ability modifier for score 15 is 2', () => {
+      expect(abilityModifier(15)).toEqual(2);
+    });
+
+    test('ability modifier for score 16 is 3', () => {
+      expect(abilityModifier(16)).toEqual(3);
+    });
+
+    test('ability modifier for score 17 is 3', () => {
+      expect(abilityModifier(17)).toEqual(3);
+    });
+
+    test('ability modifier for score 18 is 4', () => {
+      expect(abilityModifier(18)).toEqual(4);
+    });
+
+    test('ability score less than 3 throws error', () => {
+      expect(() => abilityModifier(2)).toThrow(
+        new Error('Ability scores must be at least 3')
       );
     });
 
-    test('negative square raises an exception', () => {
-      expect(() => square(-1)).toThrow(
-        new Error('square must be between 1 and 64')
-      );
-    });
-
-    test('square greater than 64 raises an exception', () => {
-      expect(() => square(65)).toThrow(
-        new Error('square must be between 1 and 64')
+    test('ability score greater than 18 throws error', () => {
+      expect(() => abilityModifier(19)).toThrow(
+        new Error('Ability scores can be at most 18')
       );
     });
   });
 
-  test('returns the total number of grains on the board', () => {
-    expect(total().toString()).toEqual('18446744073709551615');
+  describe('Ability scores calculated properly', () => {
+    test('ability within range', () => {
+      expect(Character.rollAbility()).toBeLessThanOrEqual(18);
+      expect(Character.rollAbility()).toBeGreaterThanOrEqual(3);
+    });
+
+    test('ability scores only calculated once', () => {
+      const Drizzt = new Character();
+
+      expect(Drizzt.strength).toEqual(Drizzt.strength);
+    });
+  });
+
+  describe('Random character is valid', () => {
+    test('character is valid', () => {
+      const Drizzt = new Character();
+
+      expect(Drizzt.strength).toBeLessThanOrEqual(18);
+      expect(Drizzt.strength).toBeGreaterThanOrEqual(3);
+      expect(Drizzt.dexterity).toBeLessThanOrEqual(18);
+      expect(Drizzt.dexterity).toBeGreaterThanOrEqual(3);
+      expect(Drizzt.constitution).toBeLessThanOrEqual(18);
+      expect(Drizzt.constitution).toBeGreaterThanOrEqual(3);
+      expect(Drizzt.intelligence).toBeLessThanOrEqual(18);
+      expect(Drizzt.intelligence).toBeGreaterThanOrEqual(3);
+      expect(Drizzt.wisdom).toBeLessThanOrEqual(18);
+      expect(Drizzt.wisdom).toBeGreaterThanOrEqual(3);
+      expect(Drizzt.charisma).toBeLessThanOrEqual(18);
+      expect(Drizzt.charisma).toBeGreaterThanOrEqual(3);
+
+      expect(Drizzt.hitpoints).toEqual(
+        10 + abilityModifier(Drizzt.constitution)
+      );
+    });
   });
 });
